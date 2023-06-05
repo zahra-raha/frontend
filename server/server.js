0e06505b7327d6d2bdb0062f2d95310c6cef0a7a
zahra-raha <raha.zahra1375@gmail.com>
2023-06-05 14:54:50 +0430

    edit procfile

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(publicPath, 'index.js'));
  });
 
 app.listen(port, () => {
