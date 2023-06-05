1d29ee2c7e6e542ab89173a7f4519f5fdb8f8867
zahra-raha <raha.zahra1375@gmail.com>
2023-06-05 13:16:41 +0430

    Add procfile

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosRequest = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://backend-market.herokuapp.com/",
   headers: {
     Authorization : `Token ${sessionStorage.getItem("token")}`,
   }
